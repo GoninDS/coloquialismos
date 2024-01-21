e8e491f501285080dc31d4a0e062bd337a2d7f4f
Luis <luisdi2509@gmail.com>
2024-01-21 09:20:02 -0600

    Added logic to handle strings sent to StringCleaner

--- src/data cleaner/DataCleaner.java
+++ src/data cleaner/DataCleaner.java
@@ -7,28 +7,40 @@ public class DataCleaner {
   private String fileName;
   File file;
   Scanner scanner;
+  StringCleaner stringCleaner;
 
   // Methods
   public DataCleaner(String fileName) {
     this.fileName = fileName;
+    this.stringCleaner = new StringCleaner();
   }
 
   public void cleanFile() {
     try {
       this.file = new File(this.fileName);
       this.scanner = new Scanner(file);
-      this.readFile();
+      this.processFile();
     } catch (FileNotFoundException exception) {
       System.out.println("ERROR: File not found " + fileName);
       exception.printStackTrace();
     }
   }
 
-  private void readFile() {
+  private void processFile() {
+    String accumulatedData = "";
     while (scanner.hasNextLine()) {
       String currentData = scanner.nextLine();
-      System.out.println(currentData);
+
+      if (currentData.startsWith("|")) {
+        stringCleaner.cleanString(accumulatedData);
+        accumulatedData = currentData;
+      } else {
+        // Add a space to replace the \n char
+        accumulatedData += ' ' + currentData;
+      }
     }
+    stringCleaner.cleanString(accumulatedData);
+
     scanner.close();
   }
 }
---
+++ src/data cleaner/StringCleaner.java
@@ -0,0 +1,14 @@
+public class StringCleaner {
+  // Methods
+  public StringCleaner() {}
+
+  public String cleanString(String dirtyString) {
+    if (dirtyString.equals("")) {
+      return "";
+    }
+
+    System.out.println(dirtyString);
+
+    return "";
+  }
+}
