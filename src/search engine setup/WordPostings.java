37ae9377674571da9f438cc0e31eb00bea590c9c
GoninDS <luisdi2509@gmail.com>
2024-02-17 15:27:31 -0600

    Initial implementation for WordPostings

--- src/search engine setup/WordPostings.java
+++ src/search engine setup/WordPostings.java
@@ -1,3 +1,21 @@
-public class Postings {
-  
+import java.util.HashMap;
+
+public class WordPostings {
+  // Attributes
+  HashMap<Integer, Integer> postings;
+
+  // Methods
+  public WordPostings(){
+    this.postings = new HashMap<Integer, Integer>();
+  }
+
+  public void IncreaseCount(int document) {
+    int occurrenceCount = 1;
+    // Update the occurrence count if it has already been inserted
+    if (this.postings.containsKey(document)) {
+      occurrenceCount = this.postings.get(document);
+      ++occurrenceCount;
+    }
+    this.postings.put(document, occurrenceCount);
+  }
 }
