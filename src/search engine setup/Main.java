4c055018ecee5c4ee37b9445c0894c909335deef
GoninDS <luisdi2509@gmail.com>
2024-02-19 18:46:24 -0600

    Updated WordReader to have a notion of the current line

--- src/search engine setup/Main.java
+++ src/search engine setup/Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String arg[]){
-
+    WordReader reader = new WordReader("testdata.txt");
+    String answer = "a";
+    while (!answer.equals("")) {
+      answer = reader.nextWord();
+      System.out.print("Current line: ");
+      System.out.println(reader.getCurrentLine());
+      System.out.println(answer);
+      System.out.println();
+    }
   }    
 }
--- src/search engine setup/WordReader.java
+++ src/search engine setup/WordReader.java
@@ -7,16 +7,24 @@ public class WordReader {
   private String fileName;
   private File file;
   private Scanner scanner;
+  private String[] words;
+  private int currentWord;
+  private int currentLine;
 
   // Constants
   private final String encoding = "UTF-8";
 
   // Methods
   public WordReader(String filename) {
+    // Start at -1 so that it will be set to 0 in the readNextLine method
+    this.currentLine = -1;
+    this.currentWord = -1;
     try {
       this.fileName = filename;
       this.file = new File(this.fileName);
       this.scanner = new Scanner(this.file, this.encoding);
+      // Read the first line of the file
+      this.readNextLine();
     } catch (FileNotFoundException exception) {
       System.out.println("An error occurred.");
       exception.printStackTrace();
@@ -24,6 +32,28 @@ public class WordReader {
   }
 
   public String nextWord() {
-    return this.scanner.hasNext() ? this.scanner.next() : "";
+    if (this.currentWord == this.words.length) {
+      this.readNextLine();
+    }
+    // If there is a current word, return it
+    return (this.currentWord != -1) ? this.words[this.currentWord++] : "";
+  }
+
+  private void readNextLine() {
+    // Assume that there are no more lines
+    this.currentWord = -1;
+    if (this.scanner.hasNextLine()) {
+      this.updateWordsArray();  
+    }
+  }
+
+  private void updateWordsArray() {
+    this.words = this.scanner.nextLine().split(" +");
+    this.currentWord = 0;
+    ++this.currentLine;
+  }
+
+  public int getCurrentLine() {
+    return this.currentLine;
   }
 }
